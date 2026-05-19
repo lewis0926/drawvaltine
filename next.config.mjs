c2230ed2ae97468d7a271caab7d5e58bfb2ed055
Lewis Shum <lewishum96@gmail.com>
2026-05-19 11:02:53 -0400

    Set upload body size to 10mb

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverBodySizeLimit: '10mb',
+  },
   images: {
     remotePatterns: [
       {
